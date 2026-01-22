d76126de41f4928a90a8cfd9fe3b343a3e8229eb
amalthea <oliver.hitz.7@gmail.com>
2026-01-22 20:42:40 +0100

    add trigonometric functions

--- src/generator/Primitives.cpp
+++ src/generator/Primitives.cpp
@@ -5,12 +5,15 @@
 namespace Generator {
     float *Sine(int sample_length, float frequency, const float phaseIncrement) {
         float* samples = new float[sample_length * Resonix::SAMPLE_RATE];
-        float t;
+        float t, phase;
 
         for (int i = 0; i < sample_length * Resonix::SAMPLE_RATE; i++) {
             t = static_cast<float>(i) / Resonix::SAMPLE_RATE;
-            samples[i] = Math::Sine(2.0f * Math::PI * frequency * t * 180.0f / Math::PI);
+            phase = 2.0f * Math::PI * frequency * t + phaseIncrement;
+            samples[i] = Math::Sine(phase * 180.0f / Math::PI);
         }
+
+        return samples;
     }
 
     float *Square(int sample_length, float frequency, const float phaseIncrement) {
@@ -19,9 +22,11 @@ namespace Generator {
 
         for (int i = 0; i < sample_length * Resonix::SAMPLE_RATE; i++) {
             t = static_cast<float>(i) / Resonix::SAMPLE_RATE;
-            phase = Math::fmod(t * frequency, 1.0f);
+            phase = Math::fmod(t * frequency + phaseIncrement / (2.0f * Math::PI), 1.0f);
             samples[i] = phase < 0.5f ? 1.0f : -1.0f;
         }
+
+        return samples;
     }
 
     float *Triangle(int sample_length, float frequency, const float phaseIncrement) {
@@ -30,9 +35,11 @@ namespace Generator {
 
         for (int i = 0; i < sample_length * Resonix::SAMPLE_RATE; i++) {
             t = static_cast<float>(i) / Resonix::SAMPLE_RATE;
-            phase = Math::fmod(t * frequency, 1.0f);
-            samples[i] = phase < 0.5f ? 4.0f * phase - 1.0f: 3.0f - 4.0f * phase;
+            phase = Math::fmod(t * frequency + phaseIncrement / (2.0f * Math::PI), 1.0f);
+            samples[i] = phase < 0.5f ? 4.0f * phase - 1.0f : 3.0f - 4.0f * phase;
         }
+
+        return samples;
     }
 
     float *Sawtooth(int sample_length, float frequency, const float phaseIncrement) {
@@ -41,8 +48,10 @@ namespace Generator {
 
         for (int i = 0; i < sample_length * Resonix::SAMPLE_RATE; i++) {
             t = static_cast<float>(i) / Resonix::SAMPLE_RATE;
-            phase = Math::fmod(t * frequency, 1.0f);
+            phase = Math::fmod(t * frequency + phaseIncrement / (2.0f * Math::PI), 1.0f);
             samples[i] = 2.0f * phase - 1.0f;
         }
+
+        return samples;
     }
 }
