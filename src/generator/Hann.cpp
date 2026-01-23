b07db32316cb89c65b3f2312313555218296e07b
amalthea <oliver.hitz.7@gmail.com>
2026-01-23 11:34:43 +0100

    gitignore, Hann fix

--- src/generator/Hann.cpp
+++ src/generator/Hann.cpp
@@ -3,7 +3,6 @@
 #include "Math.hpp"
 
 namespace Generator {
-
     float* Hann(int sample_length, float frequency, const float phaseIncrement) {
         float* samples = new float[static_cast<int>(sample_length * Resonix::SAMPLE_RATE)];
         float t, sine_wave, hann_window;
