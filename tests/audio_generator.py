95a60b3fe4d50f952ac30956c8c904f458535477
amalthea <oliver.hitz.7@gmail.com>
2026-01-24 21:11:27 +0100

    formant test

--- tests/audio_generator.py
+++ tests/audio_generator.py
@@ -1,7 +1,8 @@
 import resonix
 import soundfile as sf
 
-samples = resonix.generate_samples(resonix.Shape.PHASED_HANN, 5, 440.0)
+samples = resonix.generate_samples(resonix.Shape.SAWTOOTH, 5, 440.0)
+filtered_samples = resonix.formant_filter(samples, peak=0.4, mix=0.8, spread=0.2)
 
 sf.write('output/audio.wav', samples, resonix.SAMPLE_RATE)
 
