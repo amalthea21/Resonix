f0d18adcd3edfba3ea46471de61bc0a8a4c5cdd9
amalthea <oliver.hitz.7@gmail.com>
2026-01-27 14:07:13 +0100

    refactor hann into WindowFunctions.cpp

--- src/math/Trigonometry.cpp
+++ src/math/Trigonometry.cpp
@@ -1,5 +1,4 @@
 #include "../include/Math.hpp"
-#include <cmath>
 
 namespace Math {
     float Sine(float degrees) {
@@ -114,26 +113,5 @@ namespace Math {
         }
 
         return numerator / denominator;
-    }
-
-    float Hann(float n, float N) {
-        float cos_arg, last_N, inv_N_minus_1;
-        static float static_last_N, static_inv_N_minus_1;
-
-        if (isNaN(n) || isNaN(N) || N <= 1.0f) {
-            return getNaN();
-        }
-
-        static_last_N = -1.0f;
-        static_inv_N_minus_1 = 0.0f;
-
-        if (N != static_last_N) {
-            static_last_N = N;
-            static_inv_N_minus_1 = 1.0f / (N - 1.0f);
-        }
-
-        cos_arg = 360.0f * n * static_inv_N_minus_1;
-
-        return 0.5f * (1.0f - Cosine(cos_arg));
     }
 }
